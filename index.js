05a17aed12c716a19cf8ae2126d03a92cf002a5b
hails <contato.allanj@gmail.com>
2019-06-17 01:03:44 -0300

    fix script

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 const puppeteer = require('puppeteer')
 const R = require('ramda')
-const Promise = require('bluebird')
+const fs = require('fs')
+
 const args = process.argv.slice(2) || process.exit(1)
 
 const run = async (link, totalSlides) => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox']})
-    const slides = R.range(0, Number(totalSlides))
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
+  const slides = R.range(0, Number(totalSlides))
 
-    const downloadSlide = async (slide) => {
-        const page = await browser.newPage()
+  const downloadSlide = async (slide) => {
+    const page = await browser.newPage()
 
-        console.info(`Downloading slide: #${slide} of ${totalSlides - 1}`)
+    await page.setViewport({
+      width: 1920,
+      height: 1080
+    })
 
-        await page.setViewport({
-            width: 1920,
-            height: 1080
-        })
-        await page.goto(`${link}/fullscreen#/${slide}`, {waitUntil: 'networkidle0'})
-        await page.screenshot({path: `./slides/${slide}.png`})
-        return await page.close()
-    }
+    await page.goto(`${link}/fullscreen#/${slide + 1}`, { waitUntil: 'networkidle0' })
 
-    console.info(`Downloading slides from: ${link}`)
+    console.info(`Downloading slide: #${slide + 1} of ${totalSlides}`)
+    await page.screenshot({ path: `slides/${slide + 1}.png` })
+    return page.close()
+  }
 
-    return Promise.resolve(slides)
-        .map(downloadSlide)
-        .then(browser.close)
-        .catch()
+  console.info(`Downloading slides from: ${link}`)
+
+  return Promise.all(slides.map(downloadSlide))
 }
 
+const checkDownloadFolder = async () => {
+  if (!fs.existsSync('slides/')) {
+    fs.mkdirSync('slides/')
+  }
+}
+
+checkDownloadFolder()
 
 run(...args)
+  .then(() => console.log('Done!'))
+  .then(() => process.exit(0))
+  .catch(console.error)
